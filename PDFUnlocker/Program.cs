f8d96d944c759f6baac2634a3696562478a7bde7
Kishore Kumar <kishorejangid@live.com>
2017-01-07 04:15:05 +0530

    Mask password entered in Console

--- PDFUnlocker/Program.cs
+++ PDFUnlocker/Program.cs
@@ -4,8 +4,6 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PDFUnlocker
 {
@@ -15,6 +13,7 @@ namespace PDFUnlocker
         static string password;
         static void Main(string[] args)
         {
+            //Check, if we have any parameters passed.
             if(args.Length == 0)
             {
                 Console.WriteLine("Enter Input file path: ");
@@ -34,10 +33,11 @@ namespace PDFUnlocker
                     if (string.IsNullOrEmpty(password))
                     {
                         Console.WriteLine("Enter PDF password:");
-                        password = Console.ReadLine();
+                        password = ReadPassword('*');
                     }
 
-                    string newFileName = Path.Combine(Path.GetDirectoryName(inputFile),Path.GetFileNameWithoutExtension(inputFile) + "_unlocked.pdf");
+                    string newFileName = Path.Combine(Path.GetDirectoryName(inputFile),
+                        Path.GetFileNameWithoutExtension(inputFile) + "_unlocked.pdf");
                     GeneratePdf(inputFile, password, newFileName);
                 }
                 else
@@ -75,6 +75,50 @@ namespace PDFUnlocker
             {
                 Error(e.Message);
             }
+        }
+
+        /// <summary>
+        /// Like System.Console.ReadLine(), only with a mask.
+        /// </summary>
+        /// <param name="mask">a <c>char</c> representing your choice of console mask</param>
+        /// <returns>the string the user typed in </returns>
+        public static string ReadPassword(char mask)
+        {
+            const int ENTER = 13, BACKSP = 8, CTRLBACKSP = 127;
+            int[] FILTERED = { 0, 27, 9, 10 /*, 32 space, if you care */ }; // const
+
+            var pass = new Stack<char>();
+            char chr = (char)0;
+
+            while ((chr = System.Console.ReadKey(true).KeyChar) != ENTER)
+            {
+                if (chr == BACKSP)
+                {
+                    if (pass.Count > 0)
+                    {
+                        System.Console.Write("\b \b");
+                        pass.Pop();
+                    }
+                }
+                else if (chr == CTRLBACKSP)
+                {
+                    while (pass.Count > 0)
+                    {
+                        System.Console.Write("\b \b");
+                        pass.Pop();
+                    }
+                }
+                else if (FILTERED.Count(x => chr == x) > 0) { }
+                else
+                {
+                    pass.Push((char)chr);
+                    System.Console.Write(mask);
+                }
+            }
+
+            System.Console.WriteLine();
+
+            return new string(pass.Reverse().ToArray());
         }
 
         private static void Error(string msg)
